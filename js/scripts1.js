262638782fc1cda08c157f2e2ed1c8c63a5aa6f7
DoTrungHau <dotrunghau101023.st@gmail.com>
2024-02-25 17:13:47 +0700

    Huhu

--- js/scripts1.js
+++ js/scripts1.js
@@ -6,40 +6,40 @@ var btnOn3 = document.getElementById("btnon_03");
 var btnOff3 = document.getElementById("btnoff_03");
 
 btnOn1.onclick = function () {
-  document.getElementById("led_01").src = "/img/ledon.jpg";
+  document.getElementById("led_01").src = "./img/ledon.jpg";
   firebase.database().ref("/Led").update({
     Led: 1,
   });
 };
 
 btnOff1.onclick = function () {
-  document.getElementById("led_01").src = "/img/ledoff.jpg";
+  document.getElementById("led_01").src = "./img/ledoff.jpg";
   firebase.database().ref("/Led").update({
     Led: 0,
   });
 };
 btnOn2.onclick = function () {
-  document.getElementById("fan_01").src = "/img/fanon.jpg";
+  document.getElementById("fan_01").src = "./img/fanon.jpg";
   firebase.database().ref("/Fan").update({
     Fan: 1,
   });
 };
 
 btnOff2.onclick = function () {
-  document.getElementById("fan_01").src = "/img/fanoff.jpg";
+  document.getElementById("fan_01").src = "./img/fanoff.jpg";
   firebase.database().ref("/Fan").update({
     Fan: 0,
   });
 };
 btnOn3.onclick = function () {
-  document.getElementById("air_01").src = "/img/air-on.jpg";
+  document.getElementById("air_01").src = "./img/air-on.jpg";
   firebase.database().ref("/Air").update({
     Air: 1,
   });
 };
 
 btnOff3.onclick = function () {
-  document.getElementById("air_01").src = "/img/air-off.jpg";
+  document.getElementById("air_01").src = "./img/air-off.jpg";
   firebase.database().ref("/Air").update({
     Air: 0,
   });
@@ -53,24 +53,24 @@ if (!hasExecuted) {
     var led = snapshot.val().Led;
     if(led===1)
     {
-      document.getElementById("led_01").src = "/img/ledon.jpg";
-    } else  document.getElementById("led_01").src = "/img/ledoff.jpg";
+      document.getElementById("led_01").src = "./img/ledon.jpg";
+    } else  document.getElementById("led_01").src = "./img/ledoff.jpg";
   });
 
   firebase.database().ref("/Fan").on("value", function(snapshot){
     var fan = snapshot.val().Fan;
     if(fan==1)
     {
-      document.getElementById("fan_01").src = "/img/fanon.jpg";
-    } else  document.getElementById("fan_01").src = "/img/fanoff.jpg";
+      document.getElementById("fan_01").src = "./img/fanon.jpg";
+    } else  document.getElementById("fan_01").src = "./img/fanoff.jpg";
   });
 
   firebase.database().ref("/Air").on("value", function(snapshot){
     var air = snapshot.val().Air;
     if(air==1)
     {
-      document.getElementById("air_01").src = "/img/air-on.jpg";
-    } else document.getElementById("air_01").src = "/img/air-off.jpg";
+      document.getElementById("air_01").src = "./img/air-on.jpg";
+    } else document.getElementById("air_01").src = "./img/air-off.jpg";
   });
   hasExecuted = true;
 }
